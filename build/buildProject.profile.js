157065cc05405355651324aeb6173e256b65bdd0
Randy Jones <randy_jones@esri.com>
2016-03-01 15:07:36 -0500

    Update project to use bower for dependency management of the ArcGIS API for JavaScript and Grunt to wrap the dojo build

--- build/buildProject.profile.js
+++ build/buildProject.profile.js
@@ -1,58 +1,85 @@
 
-var profile = (function () { 
-	return {
-		selectorEngine : "acme",
-		basePath : "../../",
-		releaseDir : "../release",
-		releaseName : "buildProjectRelease",
-		layers : {
-			'dojo/dojo': {
-            include: [
-                'dojo/i18n', // this prevents a bunch of extra requests trying to get localization
-                'buildProject/SiteManager',
-                'esri/dijit/Attribution',
-                'dojox/gfx/path',
-                'dojox/gfx/svg',
-                'dojox/gfx/shape'
-            ],
-            includeLocales: ['en-us'],
-            customBase: true, // don't include all of the globals in dojo/main
-            boot: true // include AMD loader
-        }
-		},
-		packages: [
+var profile = {
+	basePath : "../web/js",
+	action: 'release',
+	cssOptimize: 'comments',
+	mini: true,
+	optimize: 'closure',
+	layerOptimize: 'closure',
+
+	layers : {
+		'dojo/dojo': {
+			boot: true, // include AMD loader
+			customBase: true, // don't include all of the globals in dojo/main
+			include: [
+				'dojo/i18n', // this prevents a bunch of extra requests trying to get localization
+				'buildProject/SiteManager',
+				'esri/dijit/Attribution',
+				'dojox/gfx/path',
+				'dojox/gfx/svg',
+				'dojox/gfx/shape',
+				'dojox/gfx/filters',
+				'dojox/gfx/svgext',
+				'dojo/_firebug/firebug'
+			],
+			includeLocales: ['en-us']
+		}		
+	},
+	packages: [
 		{
 			"name" : "buildProject",
-			"location" : "../buildProject"
+			"location" : "web/js/buildProject"
 		},
 		{
 			"name" : "dojo",
-			"location" : "../dojo"
+			"location" : "web/js/dojo"
 		},
 		{
 			"name" : "dijit",
-			"location" : "../dijit"
+			"location" : "web/js/dijit"
 		},
 		{
 			"name" : "dojox",
-			"location" : "../dojox"
+			"location" : "web/js/dojox"
 		},
 		{
 			"name" : "esri",
-			"location" : "../esri"
+			"location" : "web/js/esri"
 		},
 		{
 			"name" : "dgrid",
-			"location" : "../dgrid"
+			"location" : "web/js/dgrid"
 		},
 		{
 			"name" : "put-selector",
-			"location" : "../put-selector"
+			"location" : "web/js/put-selector"
 		},
 		{
 			"name": "xstyle",
-			"location" : "../xstyle"
+			"location" : "web/js/xstyle"
 		}
-		]
+	],
+	
+	useSourceMaps: false,
+	stripConsole: 'normal',
+	selectorEngine: 'acme',
+	
+	staticHasFeatures: {
+		// The trace & log APIs are used for debugging the loader, so we do not need them in the build.
+		'dojo-trace-api': false,
+		'dojo-log-api': false,
+
+		// This causes normally private loader data to be exposed for debugging. In a release build, we do not need
+		// that either.
+		'dojo-publish-privates': false,
+
+		// This application is pure AMD, so get rid of the legacy loader.
+		'dojo-sync-loader': false,
+
+		// `dojo-xhr-factory` relies on `dojo-sync-loader`, which we have removed.
+		'dojo-xhr-factory': false,
+
+		// We are not loading tests in production, so we can get rid of some test sniffing code.
+		'dojo-test-sniff': false
 	}
-})();
+};
